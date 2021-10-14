1de7d894b58faee0d0abc1f8ce82dccef31f4e87
mateusma <mateusma@student.matnat.uio.no>
2021-10-14 17:00:21 +0200

    second instance

--- project_3/main.cpp
+++ project_3/main.cpp
@@ -10,17 +10,21 @@ int main()
     double mass = 38.97;
 
     PenningTrap penning_test(B0, V0, d);
+    PenningTrap penning_test2(B0, V0, d);
 
     Particle one(charge, mass, {0,1,0}, {-1,0,0});
     //Particle two(1,1, {0,2,1}, {2,1,7});
     //Particle three(1,1,{5,8,9}, {5,1,2});
 
     penning_test.add_particle(one);
+    penning_test2.add_particle(one);
     //penning_test.add_particle(two);
     //penning_test.add_particle(three);
 
-    penning_test.evolve_forward_Euler(1e-4, 100, true);
-    penning_test.evolve_RK4(1e-4,100, true);
+    penning_test.evolve_forward_Euler(1e-3, 10, true);
+
+
+    penning_test2.evolve_RK4(1e-3,10, true);
 
     return 0;
 }
