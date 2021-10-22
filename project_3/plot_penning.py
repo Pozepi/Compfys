b8f4ca0299575a991eb1db3a1ec0e0a1700a7384
frdennis <86958788+frdennis@users.noreply.github.com>
2021-10-22 13:12:11 +0200

    plot function

--- project_3/plot_penning.py
+++ project_3/plot_penning.py
@@ -2,120 +2,223 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import animation
+import pyarma as pa
+import os
+import glob
+
+figure_path = 'figures/'
+
+if not os.path.isdir(figure_path):
+    os.mkdir(figure_path)
+
+# Collect correct path strings
+path = 'output_files/'
+single_particle_paths = []; [single_particle_paths.append(mypath+'/'+mypath.split('/')[-1]) for mypath in glob.glob(path+'1_*')] #os.listdir(path)
+two_particle_paths = []; [two_particle_paths.append(mypath+'/'+mypath.split('/')[-1]) for mypath in glob.glob(path+'2_*')] #os.listdir(path)
+n_particle_paths = []; [n_particle_paths.append(mypath+'/'+mypath.split('/')[-1]) for mypath in glob.glob(path+'n_*')] #os.listdir(path)
 
 
-""" Plot one particle"""
 
-filename = 'RK4_one_particle'#'forward_euler'
 
-def find_values(filename, particle_jump=0):
-    t = []
-    rx = []; ry = []; rz = []
-    vx = []; vy = []; vz = []
-    i = 0
-    with open(filename+'.txt', 'r') as file:
-        for line in file.readlines():
-            l = np.array(line.split())
+""" Analytical """
+q = 1
+B0 = 9.65e1
+V0 = 9.65e8
+d  = 1e4
+m  = 38.97
 
-            if len(l)==0:
-                # then we have reacha new quantity
-                i += 1
-            elif len(l)==1:
-                t.append(float(l[0]))
-            elif len(l)>1:
-                if particle_jump*3 > len(l):
-                    print('Particle jump index is too high for array with ', len(l)//3, ' particles')
-                    raise ValueError
-                if i%2 != 0:
-                    vx.append(float(l[particle_jump*3 + 0]))
-                    vy.append(float(l[particle_jump*3 + 1]))
-                    vz.append(float(l[particle_jump*3 + 2]))
-                if i%2 == 0:
-                    rx.append(float(l[particle_jump*3 + 0]))
-                    ry.append(float(l[particle_jump*3 + 1]))
-                    rz.append(float(l[particle_jump*3 + 2]))
+omega0 = q*B0/m
+omegaz = np.sqrt(2*q*V0/(m*d**2))
 
-    return t, rx, ry, rz, vx, vy, vz
+omega_pluss = 0.5*(omega0 + np.sqrt(omega0**2 - 2*omegaz**2))
+omega_minus = 0.5*(omega0 - np.sqrt(omega0**2 - 2*omegaz**2))
 
+def x(t, v0, x0):
+    # v0 initial velocity in y
+    # x0 initial position in x
+    A_pluss = (v0+x0*omega_minus)/(omega_minus - omega_pluss)
+    A_minus = -(v0+x0*omega_pluss)/(omega_minus - omega_pluss)
+    return A_pluss*np.cos(omega_pluss*t) + A_minus*np.cos(omega_minus*t)
+
+def y(t, v0, x0):
+    # v0 initial velocity in y
+    # x0 initial position in x
+    A_pluss = (v0+x0*omega_minus)/(omega_minus - omega_pluss)
+    A_minus = -(v0+x0*omega_pluss)/(omega_minus - omega_pluss)
+    return -A_minus*np.sin(omega_minus*t) - A_pluss*np.sin(omega_pluss*t)
+
+def z(t, z0):
+    return z0*np.cos(omegaz*t)
     
-#t, rx, ry, rz, vx, vy, vz = find_values(filename)
+
+
+
+""" Extract one particle coords """
+def find_values_single(filename):
+    t = pa.mat()
+    r = pa.mat()
+    v = pa.mat()
+
+    t.load(filename+'_t.txt')
+    r.load(filename+'_pos.txt')
+    v.load(filename+'_v.txt')
+    return np.array(t)[:,0], np.array(r), np.array(v)
+
+
+
+""" Compare numerical to analytical for a single particle"""
+def plot_compare_analytical(filename1, plot=False):
+    t, r, v = find_values_single(filename1)
+    fig, [ax1, ax2] = plt.subplots(figsize=(10,5), ncols=2)
+
+    ax1.set_xlabel('X position [μm]'); ax1.set_ylabel('Y position [μm]'); ax1.grid()
+    ax1.plot(r[:,0], r[:,1], label='Numerical')
+    ax1.plot(x(t, v[0,1], r[0,0]), y(t, v[0,1], r[0,0]), label='Analytical', ls='--')
+    ax1.legend()
+    #ax1.set_title()
+
+    ax2.set_xlabel('Time [s]'); ax2.set_ylabel('Z position [μm]'); ax2.grid()
+    ax2.plot(t, r[:,2], label='Numerical')
+    ax2.plot(t, z(t, r[0,2]), label='Analytical', ls='--')
+    ax2.legend()
+
+    titlename = r''
+    if filename1[-6:-4] == 'EU':
+        titlename += r'Euler with '
+    elif filename1[-6:-4] == 'RK':
+        titlename += r'Runge Kutta 4 with '
+
+    titlename += r'dt = $10^{-%.i}$'%(int(filename1[-1:]))
+
+    ax1.set_title(titlename + ' in x-y plane')
+    ax2.set_title(titlename + ' in z-t plane')
+    plt.savefig(figure_path+filename1.split('/')[-1]+'_compare_analytic.pdf')
+    if plot:
+        plt.show()
+
+
+
+
+
+""" Find the relative error """
+def relerror(plot=False):
+    fig, ax = plt.subplots(figsize=(11, 6), ncols=2)
+
+    for pathi in single_particle_paths:
+        # numerical values        
+        myname = pathi.split('/')[-1]
+        t_n, r_n, v_n = find_values_single(pathi)
+        jumpo = 100
+        t_n = t_n[::jumpo]
+        r_n = r_n[::jumpo]
+        v_n = v_n[::jumpo]
+        # analytical values
+        x_a = x(t_n, v_n[0,1], r_n[0,0])
+        y_a = y(t_n, v_n[0,1], r_n[0,0])
+        z_a = z(t_n, r_n[0,2])
+
+        R_n = np.sqrt(r_n[:,0]**2 + r_n[:,1]**2 + r_n[:,2]**2)
+        R_a = np.sqrt(x_a**2 + y_a**2 + z_a**2)
+        relerror = abs(R_a - R_n)
+
+        if pathi[-6:-4] == 'EU':
+            labelname = 'dt = $10^{-%.i}$'%int(pathi[-1:])
+            ax[0].plot(t_n, relerror, label=labelname)
+        elif pathi[-7:-5] == 'RK':
+            labelname = 'dt = $10^{-%.i}$'%int(pathi[-1:])
+            ax[1].plot(t_n, relerror, label=labelname)
+
+    ax[0].set_title('Euler')
+    ax[1].set_title('Runge Kutta 4')
+    [[axi.set_yscale('log'), axi.legend(), axi.grid()] for axi in ax]
+    plt.savefig(figure_path+'relerror.pdf')
+    if plot:
+        plt.show()
 
 """ Plot the position """
-def plot_xy(filename1):
-    t, rx, ry, rz, vx, vy, vz = find_values(filename1)
+def plot_xy_zt(filename1,plot=False):
+    t, r, v = find_values_single(filename1)
     fig, [ax1, ax2] = plt.subplots(figsize=(10,5), ncols=2)
     ax1.set_xlabel('X position'); ax1.set_ylabel('Y position')
     ax2.set_xlabel('Time [micro seconds]'); ax2.set_ylabel('Z position')
     [axi.grid() for axi in [ax1, ax2]]
 
-    ax1.plot(rx[0], ry[0], 'rx', label='Start pos')
-    ax1.plot(rx[-1], ry[-1], 'kx', label='End pos')
-    ax1.plot(rx, ry)
+    ax1.plot(r[0,0], r[0,1], 'rx', label='Start pos')
+    ax1.plot(r[-1,0], r[-1,1], 'kx', label='End pos')
+    ax1.plot(r[:,0], r[:,1])
     ax1.legend()
-    ax2.plot(t, rz)
-    plt.show()
+    ax2.plot(t, r[:,2])
+    plt.savefig(figure_path)
+    if plot:
+        plt.show()
 
-def plot_compare(filename1, filename2):
-    t, rx, ry, rz, vx, vy, vz = find_values(filename1)
+
+
+
+""" Input filenames here not path """
+def plot_compare(filename1, filename2, plot=False):
+
+    t, r, v = find_values_single(path+filename1+'/'+filename1)
     fig, [ax1, ax2] = plt.subplots(figsize=(10,5), ncols=2)
 
     [[axi.set_xlabel('X position'), axi.set_ylabel('Y position'), axi.grid()] for axi in [ax1, ax2]]
-    ax1.plot(rx, ry)
-    ax1.plot(rx[0], ry[0], 'rx', label='Start pos')
-    ax1.plot(rx[-1], ry[-1], 'kx', label='End pos')
+    ax1.plot(r[:,0], r[:,1])
+    ax1.plot(r[0,0], r[0,1], 'rx', label='Start pos')
+    ax1.plot(r[-1,0], r[-1,1], 'kx', label='End pos')
     ax1.legend()
-    ax1.set_title(filename1)
+    if filename1[5:7] == 'EU':
+        ax1.set_title('Euler with dt = $10^{-%.i}$'%int(filename1[-1:]))
+    elif filename1[5:7] == 'RK':
+        ax1.set_title('Runge Kutta with dt = $10^{-%.i}$'%int(filename1[-1:]))
 
-    t, rx, ry, rz, vx, vy, vz = find_values(filename2)
-    ax2.plot(rx, ry)
-    ax2.plot(rx[0], ry[0], 'rx', label='Start pos')
-    ax2.plot(rx[-1], ry[-1], 'kx', label='End pos')
+    t, r, v = find_values_single(path+filename2+'/'+filename2)
+    ax2.plot(r[:,0], r[:,0])
+    ax2.plot(r[0,0], r[0,1], 'rx', label='Start pos')
+    ax2.plot(r[-1,0], r[-1,1], 'kx', label='End pos')
     ax2.legend()
-    ax2.set_title(filename2)
-    plt.show()
+    if filename2[5:7] == 'EU':
+        ax2.set_title('Euler with dt = $10^{-%.i}$'%int(filename2[-1:]))
+    elif filename2[5:7] == 'RK':
+        ax2.set_title('Runge Kutta with dt = $10^{-%.i}$'%int(filename2[-1:]))
+    name = figure_path+'_'+filename1[5:7]+filename1[-1:]+'_vs_'+filename2[5:7]+filename2[-1:]+'.pdf'
+    plt.savefig(name)
+    if plot:
+        plt.show()
 
-def plot_vel():
+
+
+def plot_vel(filename,plot=False,save=False):
     """ Plot the velocity over time """
+    t, r, v = find_values_single(filename)
     fig, [ax1, ax2, ax3] = plt.subplots(figsize=(20,10), ncols=3)
+    axes = [ax1, ax2, ax3]
 
-    [axi.set_xlabel('Time [micro seconds]') for axi in [ax1, ax2, ax3]]
-    ax1.set_ylabel('X velocity')
-    ax2.set_ylabel('Y velocity')
-    ax3.set_ylabel('Z velocity')
+    ax1.plot(r[:,0], v[:,0])
+    ax2.plot(r[:,1], v[:,1])
+    ax3.plot(r[:,2], v[:,2])
+
+    ax1.set_xlabel('X position [μm]'); ax1.set_ylabel('X velocity [m/s]')
+    ax2.set_xlabel('Y position [μm]'); ax2.set_ylabel('Y velocity [m/s]')
+    ax3.set_xlabel('Z position [μm]'); ax3.set_ylabel('Z velocity [m/s]')
+
+    name = filename.split('/')[-1]
+    [axi.grid() for axi in axes]
+    if name[5:7] == 'EU':
+        ax2.set_title('Euler with dt = $10^{-%.i}$'%int(name[-1:]))
+    elif name[5:7] == 'RK':
+        ax2.set_title('Runge Kutta with dt = $10^{-%.i}$'%int(name[-1:]))
+    if plot:
+        plt.show()
+    if save:
+        
+        savename = figure_path+name[5:7]+filename[-1:]+'_phasespace.pdf'
+        plt.savefig(savename)
 
 
-def plot_multiple_particles(filename, animate=False, anisave=False):
-    t = []
-    rx = []; ry = []; rz = []
-    vx = []; vy = []; vz = []
-    i = 0
-    with open(filename+'.txt', 'r') as file:
-        for line in file.readlines():
-            # get data
-            l = np.array(line.split())
 
-            # Stage 1: The time array
-            if len(l)==1:
-                t.append(float(l[0]))
-
-            # Stage 2: approaching the velocity
-            elif len(l)==0:
-                # then we have reacha new quantity
-                i += 1
-
-            # Stage 3: append velocity and position
-            elif len(l)>1:
-                particle_N = len(l)//3
-                for k in range(0,len(l),3):
-                    if i%2 != 0:
-                        vx.append(float(l[k+0])) # vx1p1, vx1p2, vx1p3, ..., vx2p1, vx2p2, vx3p3, .... 
-                        vy.append(float(l[k+1]))
-                        vz.append(float(l[k+2]))
-                    if i%2 == 0:
-                        rx.append(float(l[k+0]))
-                        ry.append(float(l[k+1]))
-                        rz.append(float(l[k+2]))
-    
+def plot_multiple_particles(filename, animate=False, anisave=False, plot):
+    t, r, v = find_values_single(filename)
+    particle_N = len(r)//3
     rx_i = []
     ry_i = []
     rz_i = []
@@ -126,9 +229,9 @@ def plot_multiple_particles(filename, animate=False, anisave=False):
         #plot both particles
         #ax.plot(rx_i[0], ry_i[-1], 'x')
         #ax.plot3D(rx_i, ry_i, rz_i, label='Particle %.i'%i)
-        rx_i.append(rx[i::particle_N])
-        ry_i.append(ry[i::particle_N])
-        rz_i.append(rz[i::particle_N])
+        rx_i.append(r[:,i::particle_N])
+        ry_i.append(r[:,i::particle_N])
+        rz_i.append(r[:,i::particle_N])
 
     #line = []
     # set up first frame
@@ -142,6 +245,17 @@ def plot_multiple_particles(filename, animate=False, anisave=False):
         ax.set_xlim(-d, d)
         ax.set_ylim(-d, d)
         plt.show()
+    
+    elif plot:
+        fig = plt.figure()
+        ax = fig.add_sibplot(projection='3d')
+        ax.set_xlabel('X position [μm]')
+        ax.set_ylabel('Y position [μm]')
+        ax.set_zlabel('Z position [μm]')
+        for i in range(particle_N):
+            ax.plot(rx_i[i], ry_i[i])
+
+        
 
     elif animate:
         fig = plt.figure()
@@ -197,52 +311,9 @@ def plot_multiple_particles(filename, animate=False, anisave=False):
             ani.save('penning.mp4', writer=FFwriter)
         plt.show()
     
-
-q = 1
-B0 = 9.65e1
-V0 = 9.65e8
-d  = 1e4
-m  = 38.97
-
-omega0 = q*B0/m
-omegaz = np.sqrt(2*q*V0/(m*d**2))
-
-omega_pluss = 0.5*(omega0 + np.sqrt(omega0**2 - 2*omegaz**2))
-omega_minus = 0.5*(omega0 - np.sqrt(omega0**2 - 2*omegaz**2))
-
-def x(t, v0, x0):
-    # v0 initial velocity in y
-    # x0 initial position in x
-    A_pluss = (v0+x0*omega_minus)/(omega_minus - omega_pluss)
-    A_minus = -(v0+x0*omega_pluss)/(omega_minus - omega_pluss)
-    return A_pluss*np.cos(omega_pluss*t) + A_minus*np.cos(omega_minus*t)
-
-def y(t, v0, x0):
-    # v0 initial velocity in y
-    # x0 initial position in x
-    A_pluss = (v0+x0*omega_minus)/(omega_minus - omega_pluss)
-    A_minus = -(v0+x0*omega_pluss)/(omega_minus - omega_pluss)
-    return -A_minus*np.sin(omega_minus*t) - A_pluss*np.sin(omega_pluss*t)
-
-def z(t, z0):
-    return z0*np.cos(omegaz*t)
-
-def plot_compare_analytical(filename1):
-    t, rx, ry, rz, vx, vy, vz = find_values(filename1)
-    t = np.array(t)
-    fig, [ax1, ax2] = plt.subplots(figsize=(10,5), ncols=2)
-
-    ax1.set_xlabel('X position [μm]'); ax1.set_ylabel('Y position [μm]'); ax1.grid()
-    ax1.plot(rx, ry, label='Numerical')
-    ax1.plot(x(t, vy[0], rx[0]), y(t, vy[0], rx[0]), label='Analytical', ls='--')
-    ax1.legend()
-    #ax1.set_title()
-
-    ax2.set_xlabel('Time [s]'); ax2.set_ylabel('Z position [μm]'); ax2.grid()
-    ax2.plot(t, rz, label='Numerical')
-    ax2.plot(t, z(t, rz[0]), label='Analytical', ls='--')
-    ax2.legend()
-    plt.show()
+#[plot_compare_analytical(pathi) for pathi in single_particle_paths]
+#relerror()
+[plot_vel(pathi, save=True) for pathi in single_particle_paths]
 
 #plot_xy('RK4_one_particle')
 
@@ -252,4 +323,4 @@ def plot_compare_analytical(filename1):
 #plot_compare_analytical('RK4_against_analytical')
 
 #plot_multiple_particles('RK4_random_test' ,animate=True)
-plot_multiple_particles('RK4_two_particles_interaction_z_start' ,animate=True)
+#plot_multiple_particles('RK4_two_particles_interaction_z_start' ,animate=True)
