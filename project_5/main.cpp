ae536becfa992afdf103e6b94caedaac0571a185
frdennis <ctrl2000.df@gmail.com>
2021-12-14 18:19:40 +0100

    ff

--- project_5/main.cpp
+++ project_5/main.cpp
@@ -14,22 +14,20 @@ int main()
     // double slit with no potential
     //Particle myinstance(h, dt, 0.008, 0.25, 0.5, 0.05, 0.05, 200, 0.0, 2, 0, "7");
     //Particle myinstance2(h, dt, 0.008, 0.25, 0.5, 0.05, 0.1, 200, 0.0, 2, 1e10, "7_2");
-
     // double slit with potential
-    Particle myinstance3(h, dt, 0.002, 0.25, 0.5, 0.05, 0.2, 200, 0.0, 3, 1e10, "8");
+    //Particle myinstance3(h, dt, 0.002, 0.25, 0.5, 0.05, 0.2, 200, 0.0, 2, 1e10, "8");
     // no slit
     // Particle myinstance(h, dt, 0.008, 0.25, 0.5, 0.05, 0.05, 200, 0.0, 0, 1e4);
     //myinstance.simulate_system();
     //myinstance2.simulate_system();
-    
     //myinstance3.simulate_system();
 
-    Particle myinstance4(h, dt, 0.002, 0.25, 0.5, 0.05, 0.2, 200, 0.0, 3, 1e10, "9");
-    Particle myinstance5(h, dt, 0.002, 0.25, 0.5, 0.05, 0.2, 200, 0.0, 1, 1e10, "9");
-    myinstance4.simulate_system();
-    myinstance5.simulate_system();
+    //Particle myinstance4(h, dt, 0.002, 0.25, 0.5, 0.05, 0.2, 200, 0.0, 3, 1e10, "9");
+    //Particle myinstance5(h, dt, 0.002, 0.25, 0.5, 0.05, 0.2, 200, 0.0, 1, 1e10, "9");
+    //myinstance4.simulate_system();
+    //myinstance5.simulate_system();
 
-    Particle myinstancex(h, dt, 0.008, 0.25, 0.5, 0.05, 0.1, 200, 0.0, 0, 1e5, "X");
+    Particle myinstancex(h, dt, 0.008, 0.25, 0.5, 0.05, 0.1, 200, 0.0, 0, 0.5e5, "X");
     myinstancex.simulate_system();
 
     return 0;
